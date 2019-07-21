855bf01772ee9584900da6ffc54dd3c306aec453
yashsartanpara <yashsartanpara@gmail.com>
2019-07-21 12:28:10 +0530

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options : {
+        spaceId : `7rxt65kfi0x8`,
+        accessToken : `6QDaWU894yu7JQRP_U3OzrpqhkqU-qDk98AjQKPz4-w`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/pages/quote.js
@@ -0,0 +1,80 @@
+import React from "react"
+import "./style.css"
+
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+import BodyClassName from 'react-body-classname';
+class Quote extends React.Component {
+
+    state = {
+        allQuotes: [],
+        quote: {},
+        color: ""
+    }
+
+    newQuote(quotes) {
+        let min = 0,
+            max = quotes.length,
+            quote,
+            random = Math.floor(Math.random() * (+max - +min)) + +min,
+            color = this.randomColor()
+
+
+        quote = quotes[random]
+
+        this.setState({ quote: quote.node ,color })
+    }
+    randomColor() {
+
+        let rc = "#";
+        for (let i = 0; i < 6; i++) {
+            rc += Math.floor(Math.random() * 16).toString(16);
+        }
+        return rc;
+    }
+    componentWillMount() {
+        this.setState({ allQuotes: this.props.quote })
+        this.newQuote(this.props.quote)
+    }
+
+    render() {
+        console.log(this.state.color)
+        return (
+            
+            <div align="center" className='container' css={css`background-color : ${this.state.color};`}>
+                <BodyClassName className={css`background-color : ${this.state.color};`}></BodyClassName>
+                <div className="newquote row valign-wrapper">
+                    <div className="col s6 offset-s1 valign">
+                        <div className="card blue-grey darken-1">
+                            <div className="card-content white-text">
+                                <p className="p1">{
+                                    //console.log(this.state.quote)
+                                    this.state.quote.quote.quote
+                                }</p>
+                                <p className="p2 col s6 offset-s6">- {this.state.quote.author}</p>
+                            </div>
+                            <div>
+                                <div className="fl">
+                                    <div className="row">
+                                        <div className="col twit">
+                                            <button className="btn btn1" id="tweetit" css={css`background-color : ${this.state.color} !important;`}>Tweet</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="fr">
+                                    <div className="row">
+                                        <div className="col s6 offset-s6 topm">
+                                            <button className="btn btn2" id="quoteGETAJAX" css={css`background-color : ${this.state.color} !important;`} onClick={e => this.newQuote(this.state.allQuotes)}>New</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quote
